f376c109c05b8a2fd7e487a1d9847cae3410fb00
Jeothish <jeothishsenthil@gmail.com>
2025-10-12 19:16:58 +0100

    Updated naviagtion bar to only show relevant pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,12 +11,11 @@ import "../Styles/Navbar.css"
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Link to="/" className="nav-link">Expenses</Link>
-      <Link to="/dashboard" className="nav-link">Dashboard</Link>
-      <Link to="/budget" className="nav-link">Budgets</Link>
+      <Link to="/" className="nav-link">Dashboard</Link>
+      <Link to="/finance" className="nav-link">Financial Management</Link>
       <Link to="/analysis" className="nav-link">Analysis</Link>
       <Link to="/AI" className="nav-link">AI Insights</Link>
-      <Link to="/finance" className="nav-link">Financial Management</Link>  
+      
     </div>
   )
 }
