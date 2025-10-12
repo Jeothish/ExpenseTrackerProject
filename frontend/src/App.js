03906653da7e162412075f6646f0a2afb0a1c8ae
Jeothish <jeothishsenthil@gmail.com>
2025-10-12 19:41:02 +0100

    Removed page imports for old pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import "./Styles/List.css"
 import "./Styles/index.css"
 import Navbar from "./components/Navbar"
 import {Routes, Route} from "react-router-dom"
-import ExpenseTracker from "./Pages/ExpenseTracker"
+
 import { DataProvider } from "./Context/DataContext"
 import Analysis from "./Pages/Analysis"
-import Budget from "./Pages/Budget"
+
 import AI from "./Pages/AI"
 import FinancialManagement from "./Pages/FinancialManagement"
 import Dashboard from "./Pages/Dashboard"
